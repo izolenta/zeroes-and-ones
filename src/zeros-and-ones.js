a9706d590e96651fe4b10a96cb530cbfe98a6303
Alexey Sharov <alexey@sharov.io>
2022-05-10 09:39:05 +0200

    Initial

---
+++ src/zeros-and-ones.js
@@ -0,0 +1,47 @@
+import brain from "brain.js";
+import {convolution} from "brain.js/dist/src/layer/index.js";
+
+const config = {
+  hiddenLayers: [8],
+  activation: 'sigmoid',
+};
+
+const net = new brain.NeuralNetwork(config);
+
+net.train([
+  { input: [0, 0, 0, 0, 0], output: [0] },
+  { input: [0, 0, 0, 0, 1], output: [0] },
+  { input: [0, 0, 0, 1, 0], output: [0] },
+  { input: [0, 0, 0, 1, 1], output: [0] },
+  { input: [0, 0, 1, 0, 0], output: [0] },
+  { input: [0, 0, 1, 0, 1], output: [0] },
+  { input: [0, 0, 1, 1, 0], output: [0] },
+  { input: [0, 0, 1, 1, 1], output: [1] },
+  { input: [0, 1, 0, 0, 0], output: [0] },
+  { input: [0, 1, 0, 0, 1], output: [0] },
+  { input: [0, 1, 0, 1, 0], output: [0] },
+  { input: [0, 1, 0, 1, 1], output: [1] },
+  { input: [0, 1, 1, 0, 0], output: [0] },
+  { input: [0, 1, 1, 0, 1], output: [1] },
+  { input: [0, 1, 1, 1, 0], output: [1] },
+  { input: [0, 1, 1, 1, 1], output: [1] },
+  { input: [1, 0, 0, 0, 0], output: [0] },
+  { input: [1, 0, 0, 0, 1], output: [0] },
+  { input: [1, 0, 0, 1, 0], output: [0] },
+  { input: [1, 0, 0, 1, 1], output: [1] },
+  { input: [1, 0, 1, 0, 0], output: [0] },
+  { input: [1, 0, 1, 0, 1], output: [1] },
+  { input: [1, 0, 1, 1, 0], output: [1] },
+  { input: [1, 0, 1, 1, 1], output: [1] },
+  { input: [1, 1, 0, 0, 0], output: [0] },
+  { input: [1, 1, 0, 0, 1], output: [1] },
+  { input: [1, 1, 0, 1, 0], output: [1] },
+  { input: [1, 1, 0, 1, 1], output: [1] },
+  { input: [1, 1, 1, 0, 0], output: [1] },
+  { input: [1, 1, 1, 0, 1], output: [1] },
+  { input: [1, 1, 1, 1, 0], output: [1] },
+  { input: [1, 1, 1, 1, 1], output: [1] },
+]);
+
+const output = net.run([0, 0, 0, 0, 1]);
+console.log(output);
